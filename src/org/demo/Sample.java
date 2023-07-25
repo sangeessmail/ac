6b42e3e93d51f761485386b4caaef948ecec4d13
sangeessmail <sangeessmail@gmail.com>
2023-07-25 16:20:20 +0530

    code added

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,6 +1,10 @@
 package org.demo;
 
 public class Sample {
+	private void m2() {
+		System.out.println("abi code added");
+
+	}
 public static void main(String[] args) {
 	System.out.println("msg");
 }
