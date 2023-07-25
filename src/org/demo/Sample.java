4d72b1602a7196ee677a75c95b99bf75f6db21ba
sangeessmail <sangeessmail@gmail.com>
2023-07-25 16:15:24 +0530

    abi code added

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -1,7 +1,13 @@
 package org.demo;
 
 public class Sample {
+	private void m1() {
+		System.out.println("anu code added");
+
+	}
 public static void main(String[] args) {
 	System.out.println("msg");
+	
+	
 }
 }
